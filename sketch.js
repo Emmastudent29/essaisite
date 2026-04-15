07b8846c66d3adf1e8d1723dbb32fcf78a1a36c8
Emmastudent29 <emmalilyparko@gmail.com>
2026-04-15 14:37:36 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,42 +1,29 @@
-function setup() {
-  createCanvas(620, 420);
- }
-
-function draw() {
-  background(220);
-  for (let i = 0; i<15; i += 1) {
-    for (let j = 0; j<10; j += 1) {
-      if (i!==int((mouseX-10)/40) || j!==int((mouseY-10)/40))
-          {
-          couleur = color(0,255,0);
-          fill(couleur);
-          rect(40*i+10,40*j+10,40,40);
-          line( 40*i+20,40*j+20,40*i+40,40*j+40);
-          line( 40*i+40,40*j+20,40*i+20,40*j+40);
-          }
-      else if (j<5) {couleur = color(255,255,0);
-            fill(couleur);
-            rect(40*i+10,40*j+10,40,40);
-            fill(color(0,0,250));
-            rect(40*i+20,40*j+20,20,20);}
-      else if (i<8) {couleur = color(255,255,0);
-            fill(couleur);
-            rect(40*i+10,40*j+10,40,40);
-            line( 40*i+20,40*j+20,40*i+40,40*j+40);
-            line( 40*i+30,40*j+20,40*i+40,40*j+30);
-            line( 40*i+20,40*j+20,40*i+40,40*j+20);
-            line( 40*i+40,40*j+20,40*i+40,40*j+40);
-            }
-        else
-          {
-            couleur = color(255,0,0);
-            fill(couleur);
-            rect(40*i+10,40*j+10,40,40);
-            fill(color(0,0,250));
-            circle(40*i+30,40*j+30,20);
-          }
-          }
-      }
-      
-     
+// Function for first canvas
+function sketch1(p) {
+  p.setup = function () {
+    p.createCanvas(720, 200);
+    p.background(0);
+  };
+  p.draw = function () {
+    p.circle(p.mouseX, p.mouseY, 50);
+  };
 }
+
+// Run first p5 instance
+new p5(sketch1);
+
+// Function for second canvas
+function sketch2(p) {
+  p.setup = function () {
+    p.createCanvas(720, 200);
+    p.background(255);
+    p.fill(0);
+    p.stroke(255);
+  };
+  p.draw = function () {
+    p.square(p.mouseX, p.mouseY, 50);
+  };
+}
+
+// Run second p5 instance
+new p5(sketch2);
